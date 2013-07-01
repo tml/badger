237ebb2e395e668f56f130b7fecb7aa7cdfcb6cb
John Driscoll <johnoliverdriscoll@gmail.com>
2013-07-01 01:11:06 -0400

    Major rewrite to support JSON is mostly done

--- include/badger.h
+++ include/badger.h
@@ -1,60 +1,130 @@
-/* Copyright (c) 2013 John Driscoll */
+/*
+  Copyright 2013 John Driscoll
+   
+  This file is part of Badger.
+
+  Badger is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Badger is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Badger.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef BADGER_H
 #define BADGER_H
 
+/*
+  The badge data structure.  Use bdgr_badge_free to release resources.
+ */
 typedef struct {
-    unsigned char*    name;
-    unsigned long int name_len;
+    char*             id;
     unsigned char*    token;
     unsigned long int token_len;
     unsigned char*    signature;
     unsigned long int signature_len;
 } bdgr_badge;
 
-typedef dsa_key bdgr_key;
+/*
+  The badger key container.  A wrapper for libtomcrypt dsa_key.
+  Use bdgr_key_free to release resources.
+ */
+typedef struct {
+    void* _impl;
+} bdgr_key;
 
-int bdgr_generate_key(
-    const unsigned char* pass,
-    const unsigned long int pass_len,
-    bdgr_key* key );
+/*
+  Creates a key using password as entropy.
+*/
+int bdgr_key_generate(
+    const char* const password,
+    bdgr_key* const key
+);
 
-int bdgr_import_key(
-    const unsigned char* data,
+/*
+  Import binary key data.  Key data must be in libtomcrypt DSA key format.
+*/
+int bdgr_key_import(
+    const unsigned char* const data,
     const unsigned long int data_len,
-    bdgr_key* key );
+    bdgr_key* const key
+);
 
-void bdgr_free_key(
-    bdgr_key* key );
+/*
+  Import key from base64 encoded character data. Key data must be in libtomcrypt
+  DSA key format.
+*/
+int bdgr_key_decode(
+    const char* const data,
+    bdgr_key* const key
+);
 
-int bdgr_sign_token(
-    const unsigned char* token,
+/*
+  Releases resources owned by key.
+*/
+void bdgr_key_free(
+    bdgr_key* const key
+);
+
+/*
+  Signs a token using a private DSA key.
+*/
+int bdgr_token_sign(
+    const unsigned char* const token,
     const unsigned long int token_len,
-    const bdgr_key* key,
-    unsigned char* signature,
-    unsgined long int* signature_len );
+    const bdgr_key* const key,
+    unsigned char* const signature,
+    unsigned long int* const signature_len
+);
 
-const bdgr_badge bdgr_make_badge(
-    const unsigned char* name,
-    const unsigned long int name_len,
-    const unsigned char* token,
+/*
+  Copies all data into a badge struct. Use bdgr_badge_free to release resources.
+*/
+int bdgr_badge_make(
+    const char* const id,
+    const unsigned char* const token,
     const unsigned long int token_len,
-    const unsigned char* signature,
-    const unsgined long int* signature_len );
+    const unsigned char* const signature,
+    const unsigned long int signature_len,
+    bdgr_badge* const badge
+);
 
-int bdgr_verify_badge(
-    const bdgr_badge* badge,
-    const bdgr_key* key,
-    int* verified );
+/*
+  Verify a badge came from public DSA key.
+*/
+int bdgr_badge_verify(
+    const bdgr_badge* const badge,
+    const bdgr_key* const key,
+    int* const verified
+);
 
-int bdgr_import_badge(
-    const unsigned char* data,
-    const long unsigned int data_len,
-    bdgr_badge* badge );
+/*
+  Import a badge from JSON.
+*/
+int bdgr_badge_import(
+    const char* const json_string,
+    bdgr_badge* const badge
+);
 
-int bdgr_export_badge(
-    const bdgr_badge* badge,
-    unsigned char* data,
-    unsigned long int data_len );
+/*
+  Export a badge to JSON.
+*/
+int bdgr_badge_export(
+    const bdgr_badge* const badge,
+    char* json_string
+);
+
+/*
+  Free resources owned by badge.
+*/
+void bdgr_badge_free(
+    bdgr_badge* const badge
+);
 
 #endif
--- src/badger.c
+++ src/badger.c
@@ -1,218 +1,362 @@
-/* Copyright (c) 2013 John Driscoll */
+/*
+  Copyright 2013 John Driscoll
+   
+  This file is part of Badger.
+
+  Badger is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Badger is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Badger.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+  TODO: Implement decent error codes
+*/
 
 #include <badger.h>
 #include <tomcrypt.h>
+#include <jansson.h>
 
 extern ltc_math_descriptor gmp_desc;
 
-/* Badge owner structure */
-typedef struct
+static void bdgr_init()
 {
-    prng_state prng;
-    unsigned char* pass;
-    
-} bdgr_id_owner;
+    ltc_mp = gmp_desc;
+}
 
-/* Will be removed */
-extern unsigned long int test_key_len;
-extern unsigned char test_key[822];
-
-/* Init a badge owner context */
-static int bdgr_open_id_owner( bdgr_id_owner* owner, const unsigned char* pass, const unsigned long int pass_len )
+int bdgr_key_generate(
+    const char* const password,
+    bdgr_key* const key
+)
 {
     int err;
+    prng_state prng;
+    char* sane_pass;
+    int pass_len = strlen( password );
 
-    /* Use gmp math library */
-    ltc_mp = gmp_desc;
+    bdgr_init();
 
-    /* Copy the pass to a properly sized space */
-    owner->pass = malloc( 64 );
-    strncpy( (char*)owner->pass, (char*)pass, pass_len );
-    memset( (char*)owner->pass + pass_len, 0, 64 - pass_len );
-    
     /* Create an rng we can seed with Alice's password */
     if ( register_prng( &rc4_desc ) == -1 ) {
-        return 1;
+        err = 1;
+        goto bdgr_key_generate_free;
     }
 
     /* Start it */
-    err = rc4_start( &owner->prng );
+    err = rc4_start( &prng );
     if ( err != CRYPT_OK) {
-        return err;
+        goto bdgr_key_generate_free;
     }
 
+    /* Sanity check */
+    if( pass_len > 64 ) {
+        goto bdgr_key_generate_free;
+    }
+
+    /* Copy the pass to a properly sized space */
+    sane_pass = malloc( 64 );
+    memcpy( sane_pass, password, pass_len );
+    memset( sane_pass + pass_len, 0, 64 - pass_len );
+
     /* Make sure the prng buffer starts fresh */
-    memset( owner->prng.rc4.buf, 0, 256 );
+    memset( prng.rc4.buf, 0, 256 );
 
     /* Seed with Alice's password */
-    err = rc4_add_entropy( owner->pass, 64, &owner->prng );
+    err = rc4_add_entropy( (unsigned char*)sane_pass, 64, &prng );
     if ( err != CRYPT_OK ) {
-        return err;
+        goto bdgr_key_generate_free;
     }
     
     /* Ready and read */
-    err = rc4_ready( &owner->prng );
+    err = rc4_ready( &prng );
     if ( err != CRYPT_OK ) {
-        return err;
+        goto bdgr_key_generate_free;
     }
 
-    return 0;
+    err = dsa_make_key(
+        &prng, find_prng( "rc4" ),
+        30, 256,
+        (dsa_key*)key->_impl );
+    if ( err != CRYPT_OK ) {
+        goto bdgr_key_generate_free;
+    }
+    
+ bdgr_key_generate_free:
+    
+    /* Scrub any copies of the password from memory */
+    memset( prng.rc4.buf, 0, 256 );
+    
+    rc4_done( &prng );
+    if( sane_pass ) {
+        memset( sane_pass, 0, 64 );
+        free( sane_pass );
+    }
+    
+    return err;
     
 }
 
-/* Free memory associated with owner id */
-static void bdgr_close_id_owner( bdgr_id_owner* owner )
-{
-    rc4_done( &owner->prng );
-    free( owner->pass );
-}
-
-/* Generate the private key for a badge owner */
-static int bdgr_private_dsa_key( dsa_key* key, bdgr_id_owner* owner )
+int bdgr_key_import(
+    const unsigned char* const data,
+    const unsigned long int data_len,
+    bdgr_key* const key
+)
 {
     int err;
-    err = dsa_make_key( &owner->prng, find_prng( "rc4" ), 30, 256, key );
-    if ( err != CRYPT_OK ) {
-        return err;
-    }
-    return 0;
-}
+    bdgr_init();
 
-/* Retrieve the public key for a name */
-static int bdgr_public_dsa_key( dsa_key* key, const unsigned char* name, const unsigned long int name_len )
-{
-    int err = dsa_import( test_key, test_key_len, key );
+    err = dsa_import( data, data_len, (dsa_key*)key->_impl );
     if( err ) {
         return err;
     }
     return 0;
 }
 
-/* Construct a badge with given token and name */
-int bdgr_make_badge(
-    const unsigned char* name,
-    const unsigned long int name_len,
-    const unsigned char* token,
-    const unsigned long int token_len,
-    const bdgr_private_key* key,
-    bdgr_badge* badge )
+int bdgr_key_decode(
+    const char* const string,
+    bdgr_key* const key
+)
 {
-    
     int err;
-    bdgr_id_owner owner;
-    dsa_key key;
-    unsigned long int token_hash_len = 2048;
-    unsigned char token_hash[ 2048 ];
-    unsigned long int head_len = name_len + token_len + 2;
-    unsigned long int string_len = *badge_len - head_len;
-
-    /* Sanity check */
-    if( *badge_len <= head_len ) {
-        err = 1;
-        goto bdgr_make_badge_free;
+    unsigned long int string_len = strlen( string );
+    unsigned long int data_len = (string_len * 1.37) + 815;
+    unsigned char* data = malloc( data_len );
+    
+    err = base64_decode(
+        (unsigned char*)string, string_len,
+        data, &data_len );
+    if( err ) {
+        goto bdgr_key_decode_free;
     }
     
-    /* Create an owner context */
-    err = bdgr_open_id_owner( &owner, pass, pass_len );
-    if( err ) goto bdgr_make_badge_free;
+    err = bdgr_key_import( data, data_len, key );
+    if( err ) {
+        goto bdgr_key_decode_free;
+    }
 
-    /* Create a signature */
-    err = bdgr_private_dsa_key( &key, &owner );
-    if( err ) goto bdgr_make_badge_free;
+ bdgr_key_decode_free:
 
-    /* Create a hash of the token */
-    err = dsa_sign_hash( token, token_len, token_hash, &token_hash_len, &owner.prng, find_prng( "rc4" ), &key );
-    if( err != CRYPT_OK ) goto bdgr_make_badge_free;
-
-    /* Write the name */
-    memcpy( badge, name, name_len );
-    badge[ name_len ] = '\n';
-
-    /* Write the plaintext token */
-    memcpy( badge + name_len + 1, token, token_len );
-    badge[ name_len + 1 + token_len ] = '\n';
-
-    /* Write an printable string of the hash */
-    err = base64_encode( token_hash, token_hash_len, badge + head_len,  &string_len );
-    if( err != CRYPT_OK ) goto bdgr_make_badge_free;
-
-    *badge_len = head_len + string_len;
-    
- bdgr_make_badge_free:
-    
-    bdgr_close_id_owner( &owner );
+    free( data );
     return err;
 }
 
-/* Verify a badge and store the name and token it contains. This has no concept of token validity. */
-int bdgr_verify_badge(
-    const unsigned char* badge,
-    const unsigned long int badge_len,
-    unsigned char* name,
-    unsigned long int* name_len,
-    unsigned char* token,
-    unsigned long int* token_len,
-    int* verified )
+void bdgr_key_free(
+    bdgr_key* const key
+)
+{
+    dsa_free( (dsa_key*)key->_impl );
+}
+
+int bdgr_token_sign(
+    const unsigned char* const token,
+    const unsigned long int token_len,
+    const bdgr_key* const key,
+    unsigned char* const signature,
+    unsigned long int* const signature_len
+)
 {
     int err;
-    dsa_key key;
-    unsigned long int token_hash_len = 2048;
-    unsigned char token_hash[ 2048 ];
-    unsigned long int token_hash_bin_len = 2048;
-    unsigned char token_hash_bin[ 2048 ];
-    unsigned char* badge_ptr = (unsigned char*)badge;
-    unsigned char* name_ptr = name;
-    unsigned char* token_ptr = token;
-    unsigned char* token_hash_ptr = token_hash;
+    prng_state prng;
+    bdgr_init();
     
-    /* Extract name */
-    while( *badge_ptr != '\n' ) {
-        if( badge_ptr == badge + badge_len ||
-            *name_len == (unsigned long int)(name_ptr - name) ) {
-            return 1;
-        }
-        *name_ptr++ = *badge_ptr++;
-    }
-    if( name_ptr == name ) {
+    if( register_prng( &fortuna_desc) == -1 ) {
         return 1;
     }
-    *name_len = name_ptr - name;
-    ++badge_ptr;
-
-    /* Extract token */
-    while( *badge_ptr != '\n' ) {
-        if( badge_ptr == badge + badge_len ||
-            *token_len == (unsigned long int)(token_ptr - token) ) {
-            return 1;
-        }
-        *token_ptr++ = *badge_ptr++;
-    }
-    if( token_ptr == token ) {
-        return 1;
-    }
-    *token_len = token_ptr - token;
-    ++badge_ptr;
-
-    /* Extract token hash */
-    while( *badge_ptr != '\n' &&
-           badge_ptr != badge + badge_len ) {
-        *token_hash_ptr++ = *badge_ptr++;
-    }
-    if( token_hash_ptr == token_hash ) {
-        return 1;
-    }
-    token_hash_len = token_hash_ptr - token_hash;
-    
-    /* Decode token hash from base64 */
-    err = base64_decode( token_hash, token_hash_len, token_hash_bin,  &token_hash_bin_len );
-    
-    /* Get the public key */
-    err = bdgr_public_dsa_key( &key, name, *name_len );
-    if( err ) {
+    err = rng_make_prng( 128, find_prng("fortuna"), &prng, NULL );
+    if( err != CRYPT_OK) {
         return err;
     }
-    
-    /* Verify */
-    return dsa_verify_hash( token_hash_bin, token_hash_bin_len, token, *token_len, verified, &key );
+    return dsa_sign_hash(
+        token, token_len,
+        signature, signature_len,
+        &prng, find_prng( "fortuna" ),
+        (dsa_key*)key->_impl );
+}
 
+int bdgr_badge_make(
+    const char* const id,
+    const unsigned char* const token,
+    const unsigned long int token_len,
+    const unsigned char* const signature,
+    const unsigned long int signature_len,
+    bdgr_badge* const badge
+)
+{
+    unsigned long int id_len = strlen( id );
+    badge->id = malloc( id_len );
+    memcpy( badge->id, id, id_len );
+    badge->token = malloc( token_len );
+    memcpy( badge->token, token, token_len );
+    badge->token_len = token_len;
+    badge->signature = malloc( signature_len );
+    memcpy( badge->signature, signature, signature_len );
+    badge->signature_len = signature_len;
+    return 0;
+}
+
+int bdgr_badge_verify(
+    const bdgr_badge* const badge,
+    const bdgr_key* const key,
+    int* const verified
+)
+{
+    bdgr_init();
+    return dsa_verify_hash(
+        badge->signature,
+        badge->signature_len,
+        badge->token,
+        badge->token_len,
+        verified,
+        (dsa_key*)key->_impl );
+}
+
+int bdgr_badge_import(
+    const char* const json_string,
+    bdgr_badge* const badge
+)
+{
+    int err;
+    json_t* root, * id, * token, * signature;
+    json_error_t error;
+    char const* tokenc, * signaturec;
+    unsigned char* tokenb = 0, * signatureb = 0;
+    unsigned long int tokenb_len, signatureb_len;
+    
+    root = json_loads( json_string, 0, &error );
+    if( !root ) {
+        /* fprintf(stderr, "error: on line %d: %s\n", error.line, error.text);*/
+        goto bdgr_badge_import_free;
+    }
+
+    id = json_object_get( root, "id" );
+    if( !json_is_string( id )) {
+        goto bdgr_badge_import_free;
+    }
+    
+    token = json_object_get( root, "token" );
+    if( !json_is_string( token )) {
+        goto bdgr_badge_import_free;
+    }
+    
+    signature = json_object_get( root, "signature" );
+    if( !json_is_string( signature )) {
+        goto bdgr_badge_import_free;
+    }
+
+    tokenc = json_string_value( token );
+    tokenb_len = strlen( tokenc );
+    tokenb = malloc( tokenb_len );
+    err = base64_decode(
+        (unsigned char*)tokenc, tokenb_len,
+        tokenb, &tokenb_len );
+    if( err ){
+        goto bdgr_badge_import_free;
+    }
+    
+    signaturec = json_string_value( signature );
+    signatureb_len = strlen( signaturec );
+    signatureb = malloc( signatureb_len );
+    err = base64_decode(
+        (unsigned char*)signaturec, signatureb_len,
+        signatureb, &signatureb_len );
+    if( err ) {
+        goto bdgr_badge_import_free;
+    }
+
+    err = bdgr_badge_make(
+        json_string_value( id ),
+        tokenb, tokenb_len,
+        signatureb, signatureb_len,
+        badge );
+    if( err ) {
+        goto bdgr_badge_import_free;
+    }
+    
+ bdgr_badge_import_free:
+
+    if( tokenb ) {
+        free( tokenb );
+    }
+    if( signatureb ) {
+        free( signatureb );
+    }
+    if( root ) {
+        json_decref( root );
+    }
+    return err;
+    
+}
+
+int bdgr_badge_export(
+    const bdgr_badge* const badge,
+    char* json_string
+)
+{
+    int err;
+    json_t* root;
+    char* tokenc, * signaturec;
+    unsigned long int tokenc_len, signaturec_len;
+    
+    tokenc_len = (badge->token_len * 1.37) + 815;
+    tokenc = malloc( tokenc_len );
+    
+    signaturec_len = (badge->signature_len * 1.37) + 815;
+    signaturec = malloc( signaturec_len );
+    
+    err = base64_encode(
+        badge->token, badge->token_len,
+        (unsigned char*)tokenc, &tokenc_len );
+    if( err ) {
+        goto bdgr_badge_export_free;
+    }
+    
+    err = base64_encode(
+        badge->signature, badge->signature_len,
+        (unsigned char*)signaturec, &signaturec_len );
+    if( err ) {
+        goto bdgr_badge_export_free;
+    }
+    
+    root = json_pack(
+        "{sssss}", /* Creeper object */
+        "id", badge->id,
+        "token", tokenc,
+        "signature", signaturec );
+    if( !root ) {
+        err = 1;
+        goto bdgr_badge_export_free;
+    }
+
+    json_string = json_dumps( root, 0 );
+    if( !json_string ) {
+        err = 1;
+        goto bdgr_badge_export_free;
+    }
+    
+ bdgr_badge_export_free:
+
+    free( tokenc );
+    free( signaturec );
+    if( root ) {
+        json_decref( root );
+    }
+    
+    return err;
+}
+
+void bdgr_badge_free(
+    bdgr_badge* const badge
+)
+{
+    free( badge->id );
+    free( badge->token );
+    free( badge->signature );
 }
